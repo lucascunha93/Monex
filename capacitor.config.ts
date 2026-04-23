98299c2a2bd99ccbaa5d98e864d4eb5fcf86ad9d
Lucas Oliveira <lucas.oliveira@be220.com>
2026-04-22 21:17:26 -0300

    First commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'monex',
+  webDir: 'www'
+};
+
+export default config;
